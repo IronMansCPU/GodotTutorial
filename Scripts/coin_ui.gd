c864a9dc39cad02eadf9024b88f152a74a6169f0
IronMansCPU <97546649+IronMansCPU@users.noreply.github.com>
2025-02-15 15:00:18 -0400

    Slime HitBox Redone
    - Added one way collision to slime kill box

--- Scripts/coin_ui.gd
+++ Scripts/coin_ui.gd
@@ -2,7 +2,8 @@ extends Control
 
 
 @onready var label: Label = $Label
-
+@onready var camera = get_viewport().get_camera_2d()  # Get the Camera2D
+var offset = Vector2(0, 0) 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	EventController.connect("coin_collected", on_event_coin_collected)
@@ -11,3 +12,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func on_event_coin_collected(value:int) -> void:
 	label.text = str(value)
+
+#func _process(delta: float) -> void:
+#	if camera:
+#		global_position = camera.global_position - (camera.get_viewport_rect().size / 2) + offset
+	
