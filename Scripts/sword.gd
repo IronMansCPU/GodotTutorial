08ff2b9327f9ac450a1a37a4f0117e4e84ba14a4
IronMansCPU <97546649+IronMansCPU@users.noreply.github.com>
2025-03-11 15:26:47 -0300

    Sword Animation Fixed
    - Swapped scale based on which way the sword was facing
    - Added a timer to stop the animation of slime taking damage after 0.5 seconds

--- Scripts/sword.gd
+++ Scripts/sword.gd
@@ -11,23 +11,19 @@ var originalPosSword
 var original_rotation
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	originalPosSword = rotate_point.position.x
-	original_rotation = rotate_point.rotation_degrees
+	originalPosSword = position
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 		if player.playerDirection == "left":
-			sprite_2d.flip_h = true
-			rotate_point.position.x = -OFFSET_X
-			animation_player.set_speed_scale(-1)
-			rotate_point.rotation_degrees = 180
-			hitbox.position.x = -20  
+			scale.x = -abs(scale.x)
+			position.x = originalPosSword.x - 8
+			
 		else:
-			sprite_2d.flip_h = false
-			rotate_point.position.x = originalPosSword 
-			animation_player.set_speed_scale(1)
+			scale.x = abs(scale.x)
+			position.x = originalPosSword.x
 		
 		if Input.is_action_just_pressed("Attack"):
 			animation_player.play("slash")
